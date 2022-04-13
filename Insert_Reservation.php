1c845064b5f885bb4b3a4577e8fba52372043ea7
Siddiqui <atifsiddiqui103@gmail.com>
2022-04-13 16:24:59 -0500

    Booking.html done

--- Insert_Reservation.php
+++ Insert_Reservation.php
@@ -12,12 +12,13 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 if(isset($_POST['submit']))
-{    
+{    $car_id = '5';
      $days = $_POST['days'];
      $location = $_POST['location'];
      $time = $_POST['time'];
-     $sql = "INSERT INTO Reservation (ID, No_of_days, Location, Pickup_time)
-     VALUES ('' , '$days','$location','$time')";
+     $sql = "INSERT INTO Reservation (ID, No_of_days, Location, Pickup_time, Car_id)
+     -- update to make availibility = 0 
+     VALUES ('' , '$days','$location','$time', $car_id)";
      if (mysqli_query($conn, $sql)) {
         echo "New record has been added successfully !";
      } else {
